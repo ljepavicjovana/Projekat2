bb07160c664d630abb45d752ad3a6f6fe5a968e3
ljepavicjovana <jljepavic5@gmail.com>
2025-10-30 16:54:34 +0100

    Add files via upload

---
+++ DrugiProjekat/src/NastavakProjekat/src/BossEnemy.java
@@ -0,0 +1,9 @@
+
+public class BossEnemy extends Enemy {
+	public BossEnemy(String type, int damage, int health, int x, int y, int width, int height) {
+        super(type, damage, health, x, y, width, height);
+}
+	@Override
+	
+	
+}
